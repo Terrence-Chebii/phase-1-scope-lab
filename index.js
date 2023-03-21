df9e29bb4a464ab7c50af6998bdad9afa4d1f68d
bigman <terrence.chebii@student.moringaschool.com>
2023-03-21 12:43:16 +0300

    done

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 var customerName = "bob"; // declaring a variable in global scope using the var keyword
 
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase(); // accessing the global customerName variable and uppercasing it
+  customerName = customerName.toUpperCase(); 
 }
 
 function setBestCustomer() {
-  bestCustomer = "not bob"; // declaring a variable in global scope and assigning it a value from inside a function
+  bestCustomer = "not bob"; 
 }
 
 function overwriteBestCustomer() {
-  bestCustomer = "maybe bob"; // changing the value of the global variable bestCustomer
+  bestCustomer = "maybe bob";
 }
 
-const leastFavoriteCustomer = "Jill"; // declaring a constant in global scope using the const keyword
+const leastFavoriteCustomer = "Jill"; 
 
 function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = "Mary"; // attempting to change the value of the constant leastFavoriteCustomer, which will result in an error
+  leastFavoriteCustomer = "Mary"; 
 }
