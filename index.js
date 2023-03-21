8361e563809b5ec89bdc4110cfe901d24fbbfa8b
bigman <terrence.chebii@student.moringaschool.com>
2023-03-21 09:00:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"; // declaring a variable in global scope using the var keyword
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // accessing the global customerName variable and uppercasing it
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"; // declaring a variable in global scope and assigning it a value from inside a function
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"; // changing the value of the global variable bestCustomer
+}
+
+const leastFavoriteCustomer = "Jill"; // declaring a constant in global scope using the const keyword
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Mary"; // attempting to change the value of the constant leastFavoriteCustomer, which will result in an error
+}
